86aba17c3ae76551686a5ece5a6585957a57d529
Dzil <ahmadfadlydziljalal@gmail.com>
2022-09-28 01:49:52 +0700

    Tetap simpan table item untuk uji coba crud

--- migrations/temp/m220912_085031_CreateItemTable.php
+++ migrations/temp/m220912_085031_CreateItemTable.php
@@ -3,35 +3,20 @@
 use yii\db\Migration;
 
 /**
- * Handles the dropping of table `{{%item}}`.
+ * Handles the creation of table `{{%item}}`.
  */
-class m220920_060821_DropItemTable extends Migration
+class m220912_085031_CreateItemTable extends Migration
 {
     /**
      * {@inheritdoc}
      */
     public function safeUp()
     {
-
-        $this->dropForeignKey('fk_detail', '{{item_detail}}');
-        $this->dropForeignKey('fk_detail_detail', '{{item_detail_detail}}');
-        
-        $this->dropIndex('idx_detail', '{{item_detail}}');
-        $this->dropIndex('idx_detail_detail', '{{item_detail_detail}}');
-
-        $this->dropTable('{{%item_detail_detail}}');
-        $this->dropTable('{{%item_detail}}');
-        $this->dropTable('{{%item}}');
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function safeDown()
-    {
         $this->createTable('{{%item}}', [
             'id' => $this->primaryKey(),
-            'name' => $this->string()->notNull()
+            'name' => $this->string()->notNull(),
+            'tanggal' => $this->date(),
+            'tanggal_waktu' => $this->dateTime()
         ]);
 
         $this->createTable('{{%item_detail}}', [
@@ -48,11 +33,30 @@ class m220920_060821_DropItemTable extends Migration
             'dropdown_item' => $this->string(),
         ]);
 
-
         $this->createIndex('idx_detail', '{{item_detail}}', 'item_id');
         $this->createIndex('idx_detail_detail', '{{item_detail_detail}}', 'item_detail_id');
 
         $this->addForeignKey('fk_detail', '{{item_detail}}', 'item_id', '{{item}}', 'id', 'CASCADE', 'CASCADE');
         $this->addForeignKey('fk_detail_detail', '{{item_detail_detail}}', 'item_detail_id', '{{item_detail}}', 'ID', 'CASCADE', 'CASCADE');
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+
+        $this->dropForeignKey('fk_detail', '{{item_detail}}');
+        $this->dropForeignKey('fk_detail_detail', '{{item_detail_detail}}');
+
+
+        $this->dropIndex('idx_detail', '{{item_detail}}');
+        $this->dropIndex('idx_detail_detail', '{{item_detail_detail}}');
+
+
+        $this->dropTable('{{%item}}');
+        $this->dropTable('{{%item_detail}}');
+        $this->dropTable('{{%item_detail_detail}}');
     }
 }
