292c20ef206b8dae276e1181257829e61b4b4649
Dzil <ahmadfadlydziljalal@gmail.com>
2022-09-29 11:32:53 +0700

    Migration pertama  untuk mongo

---
+++ migrations/mongodb/m220928_132346_CreateDatabaseTest.php
@@ -0,0 +1,17 @@
+<?php
+
+use yii\mongodb\Migration;
+
+class m220928_132346_CreateDatabaseTest extends Migration
+{
+    public function up()
+    {
+        
+
+    }
+
+    public function down()
+    {
+
+    }
+}
