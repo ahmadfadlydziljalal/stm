8482ca1ebe3084b3c8e09f24fdd8b9bfdb895e5a
Dzil <ahmadfadlydziljalal@gmail.com>
2022-10-13 19:58:39 +0700

    Perbaiki halaman update faktur, dari id => nomor_faktur

--- views/faktur/update.php
+++ views/faktur/update.php
@@ -2,13 +2,14 @@
 
 /* @var $this yii\web\View */
 /* @var $model app\models\Faktur */
+
 /* @var $modelsDetail app\models\FakturDetail */
 
 use yii\helpers\Html;
 
-$this->title = 'Update Faktur: ' . $model->id;
+$this->title = 'Update Faktur: ' . $model->nomor_faktur;
 $this->params['breadcrumbs'][] = ['label' => 'Faktur', 'url' => ['index']];
-$this->params['breadcrumbs'][] = ['label' => $model->id, 'url' => ['view', 'id' => $model->id]];
+$this->params['breadcrumbs'][] = ['label' => $model->nomor_faktur, 'url' => ['view', 'id' => $model->id]];
 $this->params['breadcrumbs'][] = 'Update';
 ?>
 <div class="faktur-update">
